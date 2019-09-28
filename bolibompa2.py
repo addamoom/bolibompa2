1d0d2147e2132482b6ce154a855a5839d2b36216
addamoom <adam.stenseke@gmail.com>
2019-09-28 15:09:32 +0200

    trasig kod per request

--- bolibompa2.py
+++ bolibompa2.py
@@ -6,14 +6,32 @@ import tkinter
 from tkinter import messagebox
 from tkinter import filedialog
 
-getcontext().prec = 2 #behövs för att tidskonverteringen för flammcuerna ska funka
+
 
 main_win = tkinter.Tk()
 main_win.geometry("0x0")
 main_win.sourceFile = 'undefined'
 
-main_win.finaleFile = filedialog.askopenfilename(parent=main_win, initialdir= "/", title='Välj Finale-filen')
+main_win.finaleFile = filedialog.askopenfilename(parent=main_win, initialdir= ".", title='Välj Finale-filen')
+main_win.shortcutfile = filedialog.askopenfilename(parent=main_win, initialdir= ".", title='Välj shortcut filen')
+
+def dmxMatchFound()
 
 
-def finaleToFlamecues()
-    
+def finaleToDmxcues()
+    getcontext().prec = 2  # behövs för att tidskonverteringen för flammcuerna ska funka
+    flammquer = []
+    i = 0
+    with open(main_win.finaleFile, newline='', encoding='utf-8') as finalef:
+        finalefile = csv.reader(finalef, delimiter=',')
+        for f_row in finalefile:
+            if len(f_row) > 2: #sök inte igenom skabbiga tomma rader
+                with open(main_win.shortcutFile, newline='', encoding='utf-8') as shorts:
+                    sc = csv.reader(shorts, delimiter=',')
+                    for sc_row in sc:
+                        if sc_row[0] == f_row[14] and sc_row[1] == f_row[21]:
+                            dmxMatchFound()
+
+
+
+
