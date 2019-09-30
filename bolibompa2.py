e61f386f7960729fe9d8668121e12772ccb61fbd
addamoom <adam.stenseke@gmail.com>
2019-09-30 17:06:23 +0200

    lightfactorydelen typ klar

--- bolibompa2.py
+++ bolibompa2.py
@@ -5,32 +5,90 @@ import re
 import tkinter
 from tkinter import messagebox
 from tkinter import filedialog
-
+import names
 
 
 main_win = tkinter.Tk()
 main_win.geometry("0x0")
-main_win.sourceFile = 'undefined'
+main_win.finaleFile = 'undefined'
+main_win.shortcutFile = 'undefined'
 
 main_win.finaleFile = filedialog.askopenfilename(parent=main_win, initialdir= ".", title='Välj Finale-filen')
-main_win.shortcutfile = filedialog.askopenfilename(parent=main_win, initialdir= ".", title='Välj shortcut filen')
+main_win.shortcutFile = filedialog.askopenfilename(parent=main_win, initialdir= ".", title='Välj shortcut filen')
 
-def dmxMatchFound()
+pyrocues = []
+dmxques = []
 
 
-def finaleToDmxcues()
+def finale_import():
+    #prereq: finalefile, shortcutfile
+    #output: pyrocues array, dmxques
+
     getcontext().prec = 2  # behövs för att tidskonverteringen för flammcuerna ska funka
-    flammquer = []
+
     i = 0
+    flag = 0
     with open(main_win.finaleFile, newline='', encoding='utf-8') as finalef:
         finalefile = csv.reader(finalef, delimiter=',')
         for f_row in finalefile:
             if len(f_row) > 2: #sök inte igenom skabbiga tomma rader
-                with open(main_win.shortcutFile, newline='', encoding='utf-8') as shorts:
+                with open(main_win.shortcutFile, newline='', encoding='utf-8') as shorts: #borde testa att flytta ut denna till första open statementet, nu öppnas och stängs filen jätteofta
                     sc = csv.reader(shorts, delimiter=',')
                     for sc_row in sc:
                         if sc_row[0] == f_row[14] and sc_row[1] == f_row[21]:
-                            dmxMatchFound()
+                            dmxques.append({
+                                "tid": f_row[2],
+                                "shortcut": sc_row[2]
+                            })
+                            flag = 1
+                            print("Dmxque added")
+                            break
+            if flag == 0:
+                pyrocues.append(f_row) #skulle kunna filtrera bort onödiga saker ur den här arrayen
+                print ("pyrocue added")
+
+def write_dmxcues():
+    #prereq: dmxques fylld (finale_import())
+    #output: csv-fil med dmxcues formaterade för lightfactory, lägger den i samma dir som finale filen
+    with open(re.sub('\.csv$', '', main_win.finaleFile) + 'toLightfactory.csv', 'w+', newline='') as csvfile:
+        fieldnames = ['namn', 'tid', 'shortcut', '?']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        for q in dmxques:
+            tid = Decimal(q['tid'])
+            tidfloor = int(tid)
+
+            frames = (tid - tidfloor) * 25
+            tidhms = str(datetime.timedelta(seconds=tidfloor))
+            tidhms = tidhms + ":" + str(frames)
+
+            writer.writerow({'namn': names.get_first_name(), 'tid': tidhms, 'shortcut': q['shortcut'], '?': ''})
+
+finale_import()
+write_dmxcues()
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
